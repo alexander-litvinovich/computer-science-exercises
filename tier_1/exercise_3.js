3eb59e408b70b50dd9431abbe83a49ee24199482
Alexander Litvinovich <alex@Alexanders-iMac.local>
2019-01-18 23:18:00 +0300

    ex 3

--- tier_1/exercise_3.js
+++ tier_1/exercise_3.js
@@ -14,3 +14,66 @@
     Выведите количество клеток, на которую можно поставить белую пешку, 
     чтобы она не находилась под боем черных фигур.
 */
+
+inputTable =   `* * * * * * * *
+                * * * * * * * *
+                * * B * * R * *
+                * * * * * * * *
+                * * * B * * * *
+                * * * * R * * *
+                * * * * * * * *
+                * * * * * * * *`;
+               
+
+let table = inputTable.split('\n').map((line)=>{ 
+    return line.trim().split(/\s*/) 
+});
+
+function findAttackZone(board){
+    for(let y = 0; y < 8; y++){      
+        for(let x = 0; x < 8; x++){
+            if (board[y][x] === 'R'){
+                for(let i = y-1; (i >= 0 && board[i][x] !=='R' && board[i][x]!=='B'); i--) board[i][x] = "x";
+                for(let i = y+1; (i < board.length && board[i][x] !=='R' && board[i][x]!=='B'); i++) board[i][x] = "x";
+
+                for(let i = x-1; (i >= 0 && board[y][i] !=='R' && board[y][i]!=='B'); i--) board[y][i] = "x";
+                for(let i = x+1; (i < board[y].length && board[y][i] !=='R' && board[y][i]!=='B'); i++) board[y][i] = "x";                
+            }
+
+            if (board[y][x] === 'B'){
+                for(let i = 1; (y-i >= 0 && x-i >= 0 && board[y-i][x-i] !=='R' && board[y-i][x-i]!=='B'); i++) board[y-i][x-i] = "x";
+                for(let i = 1; ((y+i < board.length && x+i < board[y].length) && board[y+i][x+i] !=='R' && board[y+i][x+i]!=='B'); i++) board[y+i][x+i] = "x";
+                
+                for(let i = 1; (y-i >= 0 && x+i < board[y].length && board[y-i][x+i] !=='R' && board[y-i][x+i]!=='B'); i++) board[y-i][x+i] = "x";
+                for(let i = 1; (y+i < board.length && x-i >= 0 && board[y+i][x-i] !=='R' && board[y+i][x-i]!=='B'); i++) board[y+i][x-i] = "x";
+            }
+        }    
+    }
+}
+
+function countSafeSquare(board){
+    result = 0;
+    for(let y = 0; y < 8; y++){      
+        for(let x = 0; x < 8; x++){
+            if (board[y][x] === '*') result++;
+        }
+    }
+    return result;
+}
+
+findAttackZone(table);
+
+function showBoard(board){
+    let boardHeader = [' '];
+    for(row=0;row<board[0].length;row++)
+        boardHeader.push(String.fromCharCode(97 + row))
+
+    console.log(boardHeader.join(' '));
+
+    for (const line in board) {
+        console.log([board.length-line, ...board[line]].join(' '));
+    }
+}
+
+//showBoard(table); — uncomment to see the board
+console.log(countSafeSquare(table));
